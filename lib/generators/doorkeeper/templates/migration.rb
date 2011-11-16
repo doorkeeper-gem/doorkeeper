fb76738381f27fe4cb62d126c1f41d45bba77dce
Felipe Elias Philipp <felipeelias@gmail.com>
2011-11-16 16:02:08 +0100

    Add all tables to migration template

--- lib/generators/doorkeeper/templates/migration.rb
+++ lib/generators/doorkeeper/templates/migration.rb
@@ -1,3 +1,28 @@
 class CreateDoorkeeperTables < ActiveRecord::Migration
+  def self.up
+    create_table :oauth_applications do |t|
+      t.string :name, :null => false
+      t.string :uid
+      t.string :secret
+      t.string :redirect_uri
+      t.timestamps
+    end
 
+    create_table :oauth_access_grants do |t|
+      t.integer  :resource_owner_id, :null => false
+      t.integer  :application_id,    :null => false
+      t.string   :token
+      t.integer  :expires_in,        :null => false
+      t.datetime :created_at,        :null => false
+    end
+
+    create_table :oauth_access_tokens do |t|
+      t.integer  :resource_owner_id, :null => false
+      t.integer  :application_id,    :null => false
+      t.string   :token
+      t.datetime :expires_at,        :null => false
+      t.datetime :revoked_at
+      t.datetime :created_at,        :null => false
+    end
+  end
 end
