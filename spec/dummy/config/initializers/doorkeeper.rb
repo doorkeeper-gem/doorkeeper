719a1d57a8d563600c15a072d11e6af5331176f7
Felipe Elias Philipp <felipeelias@gmail.com>
2011-11-18 17:36:34 +0100

    Adds dummy app initializer

---
+++ spec/dummy/config/initializers/doorkeeper.rb
@@ -0,0 +1,7 @@
+Doorkeeper.setup do |config|
+  # This block will be called to check whether the
+  # resource owner is authenticated or not
+  config.resource_owner_authenticator = proc do
+    User.find_by_id(session[:user_id])
+  end
+end
