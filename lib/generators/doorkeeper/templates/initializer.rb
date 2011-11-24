389f089a79afbd806803d837ddfe0d4f5cd56c9a
Piotr Jakubowski <piotrj@gmail.com>
2011-11-24 11:45:11 +0100

    Change initializer template
    It is more descriptive and is by default to raise an exception
    if the resource_owner_authenticator block has not been configured

--- lib/generators/doorkeeper/templates/initializer.rb
+++ lib/generators/doorkeeper/templates/initializer.rb
@@ -2,7 +2,15 @@ Doorkeeper.configure do
   # This block will be called to check whether the
   # resource owner is authenticated or not
   resource_owner_authenticator do
+    raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
     # put your resource owner authentication logic here
-    # e.g. User.find_by_id(session[:user_id])
+    # e.g. User.find_by_id(session[:user_id]) || redirect_to main_app.new_user_seesion_path
   end
+
+  # If you want to restrict the access to the web interface for
+  # adding oauth authorized applications you need to declare the
+  # block below
+  # admin_authenticator do
+  #   Admin.find_by_id(session[:admin_id]) || redirect_to main_app.new_admin_session_path
+  # end
 end
