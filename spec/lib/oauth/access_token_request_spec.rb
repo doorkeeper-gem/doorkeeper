b765a77c934bbdf851d5f2722b35d7b17095d86e
Felipe Elias Philipp <felipeelias@gmail.com>
2011-11-25 16:22:42 +0100

    Wrap specs in module Doorkeeper::OAuth to fix 1.9.3 issue

--- spec/lib/oauth/access_token_request_spec.rb
+++ spec/lib/oauth/access_token_request_spec.rb
@@ -1,90 +1,90 @@
 require "spec_helper"
 
-describe Doorkeeper::OAuth::AccessTokenRequest do
-  include Doorkeeper::OAuth
-
-  let(:client) { Factory(:application) }
-  let(:grant)  { Factory(:access_grant, :application => client) }
-  let(:params) {
-    {
-      :client_id     => client.uid,
-      :client_secret => client.secret,
-      :code          => grant.token,
-      :grant_type    => "authorization_code",
-      :redirect_uri  => client.redirect_uri
+module Doorkeeper::OAuth
+  describe AccessTokenRequest do
+    let(:client) { Factory(:application) }
+    let(:grant)  { Factory(:access_grant, :application => client) }
+    let(:params) {
+      {
+        :client_id     => client.uid,
+        :client_secret => client.secret,
+        :code          => grant.token,
+        :grant_type    => "authorization_code",
+        :redirect_uri  => client.redirect_uri
+      }
     }
-  }
 
-  describe "with a valid authorization code and client" do
-    subject { AccessTokenRequest.new(params) }
+    describe "with a valid authorization code and client" do
+      subject { AccessTokenRequest.new(params) }
 
-    before { subject.authorize }
+      before { subject.authorize }
 
-    it { should be_valid }
-    its(:access_token) { should =~ /\w+/ }
-    its(:token_type)   { should == "bearer" }
-    its(:error)        { should be_nil }
-  end
-
-  describe "with errors" do
-    def token(params)
-      AccessTokenRequest.new(params)
+      it { should be_valid }
+      its(:access_token) { should =~ /\w+/ }
+      its(:token_type)   { should == "bearer" }
+      its(:error)        { should be_nil }
     end
 
-    it "includes the error in the response" do
-      access_token = token(params.except(:grant_type))
-      access_token.error_response['error'].should == "invalid_request"
-    end
-
-    [:grant_type, :code, :redirect_uri].each do |param|
-      describe "when :#{param} is missing" do
-        subject     { token(params.except(param)) }
-        its(:error) { should == :invalid_request }
-      end
-    end
-
-    describe "when :client_id does not match" do
-      subject     { token(params.merge(:client_id => "inexistent")) }
-      its(:error) { should == :invalid_client }
-    end
-
-    describe "when :client_secret does not match" do
-      subject     { token(params.merge(:client_secret => "inexistent")) }
-      its(:error) { should == :invalid_client }
-    end
-
-    describe "when :code does not exist" do
-      subject     { token(params.merge(:code => "inexistent")) }
-      its(:error) { should == :invalid_grant }
-    end
-
-    describe "when :redirect_uri does not match with grant's one" do
-      subject     { token(params.merge(:redirect_uri => "another")) }
-      its(:error) { should == :invalid_grant }
-    end
-
-    describe "when :grant_type is not 'authorization_code'" do
-      subject     { token(params.merge(:grant_type => "invalid")) }
-      its(:error) { should == :unsupported_grant_type }
-    end
-
-    describe "when granted application does not match" do
-      subject { token(params) }
-
-      before do
-        grant.application = Factory(:application)
-        grant.save!
+    describe "with errors" do
+      def token(params)
+        AccessTokenRequest.new(params)
       end
 
-      its(:error) { should == :invalid_grant }
-    end
+      it "includes the error in the response" do
+        access_token = token(params.except(:grant_type))
+        access_token.error_response['error'].should == "invalid_request"
+      end
 
-    describe "when :code is expired" do
-      it "error is :invalid_grant" do
-        grant # create grant instance
-        Timecop.freeze(Time.now + 700.seconds) do
-          expired = token(params)
-          expired.error.should == :invalid_grant
+      [:grant_type, :code, :redirect_uri].each do |param|
+        describe "when :#{param} is missing" do
+          subject     { token(params.except(param)) }
+          its(:error) { should == :invalid_request }
+        end
+      end
+
+      describe "when :client_id does not match" do
+        subject     { token(params.merge(:client_id => "inexistent")) }
+        its(:error) { should == :invalid_client }
+      end
+
+      describe "when :client_secret does not match" do
+        subject     { token(params.merge(:client_secret => "inexistent")) }
+        its(:error) { should == :invalid_client }
+      end
+
+      describe "when :code does not exist" do
+        subject     { token(params.merge(:code => "inexistent")) }
+        its(:error) { should == :invalid_grant }
+      end
+
+      describe "when :redirect_uri does not match with grant's one" do
+        subject     { token(params.merge(:redirect_uri => "another")) }
+        its(:error) { should == :invalid_grant }
+      end
+
+      describe "when :grant_type is not 'authorization_code'" do
+        subject     { token(params.merge(:grant_type => "invalid")) }
+        its(:error) { should == :unsupported_grant_type }
+      end
+
+      describe "when granted application does not match" do
+        subject { token(params) }
+
+        before do
+          grant.application = Factory(:application)
+          grant.save!
+        end
+
+        its(:error) { should == :invalid_grant }
+      end
+
+      describe "when :code is expired" do
+        it "error is :invalid_grant" do
+          grant # create grant instance
+          Timecop.freeze(Time.now + 700.seconds) do
+            expired = token(params)
+            expired.error.should == :invalid_grant
+          end
         end
       end
     end
--- spec/lib/oauth/authorization_request_spec.rb
+++ spec/lib/oauth/authorization_request_spec.rb
@@ -1,69 +1,70 @@
 require "spec_helper"
 
-describe Doorkeeper::OAuth::AuthorizationRequest do
-  include Doorkeeper::OAuth
-
-  let(:resource_owner) { double(:resource_owner, :id => 1) }
-  let(:client)         { Factory(:application) }
-  let(:attributes) do
-    {
-      :response_type => "code",
-      :client_id     => client.uid,
-      :redirect_uri  => client.redirect_uri,
-      :scope         => "public",
-      :state         => "return-this"
-    }
-  end
-
-  describe "with valid attributes" do
-    subject { AuthorizationRequest.new(resource_owner, attributes) }
-
-    before { subject.authorize }
-
-    its(:response_type) { should == "code" }
-    its(:client_id)     { should == client.uid }
-    its(:scope)         { should == "public" }
-    its(:state)         { should == "return-this" }
-    its(:error)         { should be_nil }
-
-    describe ".success_redirect_uri" do
-      let(:query) { URI.parse(subject.success_redirect_uri).query }
-
-      it "includes the grant code" do
-        query.should =~ %r{code=\w+}
-      end
-
-      it "includes the state previous assumed" do
-        query.should =~ %r{state=return-this}
-      end
-    end
-  end
-
-  describe "with errors" do
-    before do
-      AccessGrant.should_not_receive(:create)
+module Doorkeeper::OAuth
+  describe AuthorizationRequest do
+    let(:resource_owner) { double(:resource_owner, :id => 1) }
+    let(:client)         { Factory(:application) }
+    let(:attributes) do
+      {
+        :response_type => "code",
+        :client_id     => client.uid,
+        :redirect_uri  => client.redirect_uri,
+        :scope         => "public",
+        :state         => "return-this"
+      }
     end
 
-    [:response_type, :client_id, :redirect_uri].each do |attribute|
-      describe "when :#{attribute} is missing" do
-        subject     { auth(attributes.except(attribute)) }
-        its(:error) { should == :invalid_request }
+    describe "with valid attributes" do
+      subject { AuthorizationRequest.new(resource_owner, attributes) }
+
+      before { subject.authorize }
+
+      its(:response_type) { should == "code" }
+      its(:client_id)     { should == client.uid }
+      its(:scope)         { should == "public" }
+      its(:state)         { should == "return-this" }
+      its(:error)         { should be_nil }
+
+      describe ".success_redirect_uri" do
+        let(:query) { URI.parse(subject.success_redirect_uri).query }
+
+        it "includes the grant code" do
+          query.should =~ %r{code=\w+}
+        end
+
+        it "includes the state previous assumed" do
+          query.should =~ %r{state=return-this}
+        end
       end
     end
 
-    describe "when :client_id does not match" do
-      subject     { auth(attributes.merge(:client_id => "invalid")) }
-      its(:error) { should == :invalid_client }
-    end
+    describe "with errors" do
+      before do
+        AccessGrant.should_not_receive(:create)
+      end
 
-    describe "when :redirect_uri mismatches" do
-      subject     { auth(attributes.merge(:redirect_uri => "mismatch")) }
-      its(:error) { should == :invalid_redirect_uri }
-    end
+      [:response_type, :client_id, :redirect_uri].each do |attribute|
+        describe "when :#{attribute} is missing" do
+          subject     { auth(attributes.except(attribute)) }
+          its(:error) { should == :invalid_request }
+        end
+      end
 
-    describe "when :response_type is not 'code'" do
-      subject     { auth(attributes.merge(:response_type => "invalid")) }
-      its(:error) { should == :unsupported_response_type }
+      describe "when :client_id does not match" do
+        subject     { auth(attributes.merge(:client_id => "invalid")) }
+        its(:error) { should == :invalid_client }
+      end
+
+      describe "when :redirect_uri mismatches" do
+        subject     { auth(attributes.merge(:redirect_uri => "mismatch")) }
+        its(:error) { should == :invalid_redirect_uri }
+      end
+
+      describe "when :response_type is not 'code'" do
+        subject     { auth(attributes.merge(:response_type => "invalid")) }
+        its(:error) { should == :unsupported_response_type }
+      end
+
     end
 
     def auth(attributes)
