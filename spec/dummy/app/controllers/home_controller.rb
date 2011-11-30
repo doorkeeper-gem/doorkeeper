44af71eae8ade27489fc2b873f86210a9f34dd00
Felipe Elias Philipp <felipeelias@gmail.com>
2011-11-30 17:14:26 +0100

    Add dummy sign in to dummy app

--- spec/dummy/app/controllers/home_controller.rb
+++ spec/dummy/app/controllers/home_controller.rb
@@ -3,6 +3,11 @@ class HomeController < ApplicationController
 
   end
 
+  def sign_in
+    session[:user_id] = User.first || User.create!
+    redirect_to '/'
+  end
+
   def callback
     render :text => "ok"
   end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Doorkeeper::Engine => "/oauth"
   get '/callback', :to => "home#callback"
+  get '/sign_in',  :to => "home#sign_in"
   resources :semi_protected_resources
   resources :full_protected_resources
   root :to => "home#index"
