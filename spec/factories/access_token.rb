44035302cc7a2e48d15335fe331de03c7dee04d8
Felipe Elias Philipp <felipeelias@gmail.com>
2011-12-06 10:00:11 +0100

    Fix expiration in access token factory

--- spec/factories/access_token.rb
+++ spec/factories/access_token.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :access_token do
     sequence(:resource_owner_id) { |n| n }
     application
-    expires_in { Time.now + 2.hours }
+    expires_in 2.hours
   end
 end
