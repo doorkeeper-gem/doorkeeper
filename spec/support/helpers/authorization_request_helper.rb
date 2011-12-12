356c517dff733fd3d7e5d175779a6899db13b7bd
Felipe Elias Philipp <felipeelias@gmail.com>
2011-12-12 15:20:02 +0100

    Remove stub call

--- spec/support/helpers/authorization_request_helper.rb
+++ spec/support/helpers/authorization_request_helper.rb
@@ -1,11 +1,11 @@
 module AuthorizationRequestHelper
   def resource_owner_is_authenticated(resource_owner = nil)
     resource_owner ||= User.create!
-    Doorkeeper.configuration.stub(:authenticate_resource_owner => proc { resource_owner })
+    Doorkeeper.configuration.instance_variable_set(:@authenticate_resource_owner, proc { resource_owner })
   end
 
   def resource_owner_is_not_authenticated
-    Doorkeeper.configuration.stub(:authenticate_resource_owner => proc { redirect_to("/sign_in") })
+    Doorkeeper.configuration.instance_variable_set(:@authenticate_resource_owner, proc { redirect_to("/sign_in") })
   end
 
   def client_exists(client_attributes = {})
