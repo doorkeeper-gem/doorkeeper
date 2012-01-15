0976a45c056c35968131264d91acf6000530f3a8
Felipe Elias Philipp <felipeelias@gmail.com>
2012-01-15 22:20:41 +0100

    Introduce #valid? method in ScopeChecker

--- lib/doorkeeper/oauth/helpers/scope_checker.rb
+++ lib/doorkeeper/oauth/helpers/scope_checker.rb
@@ -6,6 +6,12 @@ module Doorkeeper
           return false if current_scopes.nil? || scopes.nil?
           current_scopes.map(&:to_s).sort == scopes.split(" ").sort
         end
+
+        def self.valid?(scope, server_scopes)
+          scope.present? &&
+          scope !~ /[\n|\r|\t]/ &&
+          server_scopes.all_included?(scope)
+        end
       end
     end
   end
--- spec/lib/oauth/helpers/scope_checker_spec.rb
+++ spec/lib/oauth/helpers/scope_checker_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
+require 'active_support/core_ext/string'
 require 'doorkeeper/oauth/helpers/scope_checker'
 
 module Doorkeeper::OAuth::Helpers
-  describe ScopeChecker do
+  describe ScopeChecker, ".matches?" do
     it "true if scopes matches" do
       scopes = [:public]
       scopes_to_match = "public"
@@ -30,6 +31,39 @@ module Doorkeeper::OAuth::Helpers
     it "is false when any of attributes is nil" do
       ScopeChecker.matches?(nil, stub).should be_false
       ScopeChecker.matches?(stub, nil).should be_false
+    end
+  end
+
+  describe ScopeChecker, ".valid?" do
+    let(:server_scopes) { double :all_included? => true }
+
+    it "is valid if scope is present" do
+      ScopeChecker.valid?("scope", server_scopes).should be_true
+    end
+
+    it "is invalid if includes tabs space" do
+      ScopeChecker.valid?("\tsomething", server_scopes).should be_false
+    end
+
+    it "is invalid if scope is not present" do
+      ScopeChecker.valid?(nil, server_scopes).should be_false
+    end
+
+    it "is invalid if scope is blank" do
+      ScopeChecker.valid?(" ", server_scopes).should be_false
+    end
+
+    it "is invalid if includes return space" do
+      ScopeChecker.valid?("scope\r", server_scopes).should be_false
+    end
+
+    it "is invalid if includes new lines" do
+      ScopeChecker.valid?("scope\nanother", server_scopes).should be_false
+    end
+
+    it "is invalid if any scope is not included in server scopes" do
+      server_scopes.stub(:all_included?).and_return(false)
+      ScopeChecker.valid?("scope another", server_scopes).should be_false
     end
   end
 end
