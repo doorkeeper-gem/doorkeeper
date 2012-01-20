61a438561feff77f08ad783b3a782d6ecfc0d1eb
Felipe Elias Philipp <felipeelias@gmail.com>
2012-01-20 23:05:05 +0100

    Move integration specs for error in auth code to separated feature

--- spec/requests/authorization/authorization_request_spec.rb
+++ spec/requests/authorization/authorization_request_spec.rb
@@ -8,46 +8,6 @@ feature "Authorization Request", "when resource owner is authenticated" do
     scope_exist(:write, :description => "Update your data")
   end
 
-  context "with valid client credentials and parameters" do
-    context "with state parameter" do
-      scenario "returns the state if client access was denied" do
-        visit authorization_endpoint_url(:client => @client, :state => "return-that")
-        click_on "Deny"
-        url_should_have_param("state", "return-that")
-      end
-    end
-
-    scenario "resource owner denies client access" do
-      visit authorization_endpoint_url(:client => @client)
-
-      click_on "Deny"
-      client_should_not_be_authorized(@client)
-
-      i_should_be_on_client_callback(@client)
-      url_should_not_have_param("code")
-      url_should_have_param("error", "access_denied")
-    end
-  end
-
-  context "with invalid client credentials or redirect_uri for a code response type" do
-    after do
-      client_should_not_be_authorized(@client)
-    end
-
-    [
-      [:client_id,     :invalid_client],
-      [:redirect_uri,  :invalid_redirect_uri],
-    ].each do |error|
-      scenario "receives an error for invalid #{error.first}" do
-        parameter       = error.first
-        translation_key = error.last
-        visit authorization_endpoint_url(:client => @client, :response_type => "code", parameter => "invalid")
-        i_should_see "An error has occurred"
-        i_should_see I18n.translate translation_key, :scope => [:doorkeeper, :errors, :messages]
-      end
-    end
-  end
-
   context "with invalid client credentials or redirect_uri for a token response type" do
     after do
       client_should_not_be_authorized(@client)
@@ -66,28 +26,6 @@ feature "Authorization Request", "when resource owner is authenticated" do
       end
     end
   end
-
-  context "with invalid response type or scope" do
-    after do
-      client_should_not_be_authorized(@client)
-    end
-
-    [
-      [:response_type, :unsupported_response_type],
-      [:scope,         :invalid_scope]
-    ].each do |error|
-      scenario "receives an error for invalid #{error.first} and redirects to the code endpoint" do
-        parameter       = error.first
-        error_type = error.last
-        visit authorization_endpoint_url(:client => @client, parameter => "invalid")
-        current_params.should_not be_nil
-        current_params["error"].should == error_type.to_s
-        current_params["error_description"].should == (I18n.translate error_type, :scope => [:doorkeeper, :errors, :messages])
-        i_should_be_on_client_callback(@client)
-      end
-    end
-  end
-
 end
 
 feature "Authorization Request", "with no scopes" do
---
+++ spec/requests/flows/authorization_code_errors_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper_integration'
+
+feature 'Authorization Code Flow Errors' do
+  background do
+    config_is_set(:authenticate_resource_owner) { User.first || redirect_to('/sign_in') }
+    client_exists
+    create_resource_owner
+    sign_in
+  end
+
+  after do
+    access_grant_should_not_exists
+  end
+
+  scenario "redirects with :invalid_request error when :response_type is missing" do
+    visit authorization_endpoint_url(:client => @client, :response_type => "")
+    i_should_be_on_client_callback @client
+    url_should_have_param "error", "invalid_request"
+    url_should_have_param "error_description", translated_error_message(:invalid_request)
+  end
+
+  scenario "redirects with :unsupported_response_type error for invalid :response_type" do
+    visit authorization_endpoint_url(:client => @client, :response_type => "invalid")
+    i_should_be_on_client_callback @client
+    url_should_have_param "error", "unsupported_response_type"
+    url_should_have_param "error_description", translated_error_message(:unsupported_response_type)
+  end
+
+  [
+    [:client_id,     :invalid_client],
+    [:redirect_uri,  :invalid_redirect_uri],
+  ].each do |error|
+    scenario "displays #{error.last.inspect} error for invalid #{error.first.inspect}" do
+      visit authorization_endpoint_url(:client => @client, error.first => "invalid")
+      i_should_not_see "Authorize"
+      i_should_see_translated_error_message error.last
+    end
+
+    scenario "displays #{error.last.inspect} error when #{error.first.inspect} is missing" do
+      visit authorization_endpoint_url(:client => @client, error.first => "")
+      i_should_not_see "Authorize"
+      i_should_see_translated_error_message error.last
+    end
+  end
+
+  context 'when access was denied' do
+    scenario 'redirects with error' do
+      visit authorization_endpoint_url(:client => @client)
+      click_on "Deny"
+
+      i_should_be_on_client_callback @client
+      url_should_not_have_param "code"
+      url_should_have_param "error", "access_denied"
+      url_should_have_param "error_description", translated_error_message(:access_denied)
+    end
+
+    scenario 'redirects with state parameter' do
+      visit authorization_endpoint_url(:client => @client, :state => "return-this")
+      click_on "Deny"
+
+      i_should_be_on_client_callback @client
+      url_should_not_have_param "code"
+      url_should_have_param "state", "return-this"
+    end
+  end
+
+  context 'with scopes' do
+    background do
+      scope_exist :write, :description => "Update your data"
+    end
+
+    scenario "redirects with :invalid_scope error when scope does not exists" do
+      visit authorization_endpoint_url(:client => @client, :scope => "invalid")
+
+      i_should_be_on_client_callback @client
+      url_should_have_param "error", "invalid_scope"
+      url_should_have_param "error_description", translated_error_message(:invalid_scope)
+    end
+  end
+end
