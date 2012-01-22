60cfb92ab8519d1a88fb435ff7c2d83fc3645866
Felipe Elias Philipp <felipeelias@gmail.com>
2012-01-22 19:13:06 +0100

    Spec for implicit grant flow
    Since capybara do not make fragments accessible to the request
    specs, the implicit grant flow is tested in the controller level

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -1,9 +1,52 @@
 require 'spec_helper_integration'
 
-describe Doorkeeper::AuthorizationsController do
-  include Doorkeeper::OAuth
+describe Doorkeeper::AuthorizationsController, "implicit grant flow" do
   include AuthorizationRequestHelper
   include ModelHelper
+
+  def fragments(param)
+    fragment = URI.parse(response.location).fragment
+    Rack::Utils.parse_query(fragment)[param]
+  end
+
+  describe "POST #create" do
+    let(:client) { Factory :application }
+    let(:user)   { double(:user, :id => 9) }
+
+    before do
+      controller.stub :current_resource_owner => user
+      post :create, :client_id => client.uid, :response_type => "token", :redirect_uri => client.redirect_uri, :use_route => :doorkeeper
+    end
+
+    it "redirects after authorization" do
+      response.should be_redirect
+    end
+
+    it "redirects to client redirect uri" do
+      response.location.should =~ %r[^#{client.redirect_uri}]
+    end
+
+    it "includes access token in fragment" do
+      fragments("access_token").should == AccessToken.first.token
+    end
+
+    it "includes token type in fragment" do
+      fragments("token_type").should == "bearer"
+    end
+
+    it "includes token expiration in fragment" do
+      pending "expiration time is given by the expired time - Time.now, which leads to a weird number"
+      fragments("expires_in").should == 2.hours.to_i
+    end
+
+    it "issues the token for the current client" do
+      AccessToken.first.application_id.should == client.id
+    end
+
+    it "issues the token for the current resource owner" do
+      AccessToken.first.resource_owner_id.should == user.id
+    end
+  end
 
   describe "#new with token response type" do
     before do
@@ -50,19 +93,6 @@ describe Doorkeeper::AuthorizationsController do
       client_exists
       scope_exist(:public, :default => true, :description => "Access your public data")
       scope_exist(:write, :description => "Update your data")
-    end
-
-    it "creates a new access token and returns it in a fragment" do
-      lambda {
-        post :create, :client_id => @client.uid, :response_type => "token", :redirect_uri => @client.redirect_uri, :use_route => :doorkeeper
-        response.should be_redirect
-        uri = response.location
-        fragment_params = parse_fragment_params(uri)
-        fragment_params.should_not be_empty
-        fragment_params["access_token"].should_not be_nil
-        fragment_params["expires_in"].should_not be_nil
-        fragment_params["token_type"].should == "bearer"
-        }.should change(AccessToken, :count).by(1)
     end
 
     it "returns the existing access token in a fragment if a token exists" do
