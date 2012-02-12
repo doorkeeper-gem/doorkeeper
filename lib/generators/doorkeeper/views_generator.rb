1d43ae1fabd4322c6e39a62a7bf7521fa94c2745
Felipe Elias Philipp <felipeelias@gmail.com>
2012-02-12 19:02:21 +0100

    Add views generator

---
+++ lib/generators/doorkeeper/views_generator.rb
@@ -0,0 +1,13 @@
+module Doorkeeper
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../../../app/views/doorkeeper', __FILE__)
+
+      def manifest
+        directory 'applications', 'app/views/doorkeeper/applications'
+        directory 'authorizations', 'app/views/doorkeeper/authorizations'
+        directory 'authorized_applications', 'app/views/doorkeeper/authorized_applications'
+      end
+    end
+  end
+end
