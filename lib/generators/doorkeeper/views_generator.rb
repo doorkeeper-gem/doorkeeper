df06cd7be3c6084eac4350da750a01e19bd6ee8b
Felipe Elias Philipp <felipeelias@gmail.com>
2012-02-12 19:18:32 +0100

    Add description to views generator

--- lib/generators/doorkeeper/views_generator.rb
+++ lib/generators/doorkeeper/views_generator.rb
@@ -3,6 +3,8 @@ module Doorkeeper
     class ViewsGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../../app/views/doorkeeper', __FILE__)
 
+      desc "Copies default Doorkeeper views to your application."
+
       def manifest
         directory 'applications', 'app/views/doorkeeper/applications'
         directory 'authorizations', 'app/views/doorkeeper/authorizations'
