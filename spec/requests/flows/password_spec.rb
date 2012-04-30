2c8e8c86e60ba2705a7ccaddb7a31a29f4343964
Jaime Iniesta <jaimeiniesta@gmail.com>
2012-04-30 23:30:26 +0200

    Remove documentation that now goes on the wiki

--- spec/requests/flows/password_spec.rb
+++ spec/requests/flows/password_spec.rb
@@ -1,34 +1,5 @@
 # coding: utf-8
 
-# ========================================
-# Resource Owner Password Credentials flow
-# ========================================
-#
-# In this flow, a token is requested in exchange for the resource owner
-# credentials (username and password):
-#
-# http://tools.ietf.org/html/draft-ietf-oauth-v2-25#page-9
-# http://tools.ietf.org/html/draft-ietf-oauth-v2-25#page-34
-#
-# For instance, using the oauth2 ruby gem, we would request it like this:
-#
-#   client = OAuth2::Client.new('the_client_id', 'the_client_secret',
-#                               :site => "http://example.com")
-#   access_token = client.password.get_token('user@example.com', 'sekret')
-#
-# That will make a POST request to the OAuth providers "/oauth/token" endpoint,
-# with the params:
-#
-#   "grant_type"    => "password"
-#   "username"      => "user@example.com"
-#   "password"      => "sekret"
-#   "client_id"     => "the_client_id"
-#   "client_secret" => "the_client_secret"
-#
-# The Rails app will need to implement user authentication based on username and
-# password, and Doorkeeper will have to be configured to use this authentication
-# to get the resource owner from the credentials
-#
 # TODO: this flow should be configurable (letting Doorkeeper users decide if
 # they want to make it available)
 
