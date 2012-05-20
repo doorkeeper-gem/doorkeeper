68594cdc9506731aac41fb0d3df79290a8580e2d
Felipe Elias Philipp <felipeelias@gmail.com>
2012-05-20 19:24:11 +0200

    Credentials accepts array of methods to be executed in order

--- lib/doorkeeper/oauth/client/credentials.rb
+++ lib/doorkeeper/oauth/client/credentials.rb
@@ -2,14 +2,22 @@ module Doorkeeper
   module OAuth
     class Client
       class Credentials < Struct.new(:uid, :secret)
-        def self.from_request(request)
+        def self.from_request(request, *methods)
+          methods.inject(nil) do |credentials, method|
+            credentials = self.send method, request
+            break credentials unless credentials.blank?
+          end
+        end
+
+        def self.from_params(request)
+          new *request.parameters.values_at(:client_id, :client_secret)
+        end
+
+        def self.from_basic(request)
           authorization = request.env['HTTP_AUTHORIZATION']
-          params        = request.parameters
           if authorization.present? && authorization =~ /^Basic (.*)/m
             uid, secret = Base64.decode64($1).split(/:/, 2)
-            new(uid, secret)
-          else
-            new(params[:client_id], params[:client_secret])
+            new uid, secret
           end
         end
 
--- spec/lib/oauth/client/credentials_spec.rb
+++ spec/lib/oauth/client/credentials_spec.rb
@@ -12,37 +12,57 @@ class Doorkeeper::OAuth::Client
       Credentials.new("something", nil).should be_blank
     end
 
-    describe :from_request do
-      context 'from params' do
-        it 'returns credentials from parameters when Authorization header is not available' do
-          request = stub :env => {}, :parameters => { :client_id => client_id, :client_secret => client_secret }
-          credentials = Credentials.from_request(request)
-          credentials.uid.should    == "some-uid"
-          credentials.secret.should == "some-secret"
-        end
-
-        it 'is blank when there are no credentials' do
-          request = stub :env => {}, :parameters => {}
-          credentials = Credentials.from_request(request)
-          credentials.should be_blank
-        end
+    describe :from_params do
+      it 'returns credentials from parameters when Authorization header is not available' do
+        request = stub :parameters => { :client_id => client_id, :client_secret => client_secret }
+        credentials = Credentials.from_params(request)
+        credentials.uid.should    == "some-uid"
+        credentials.secret.should == "some-secret"
       end
 
-      context 'from Authorization header' do
-        let(:credentials) { Base64.encode64("#{client_id}:#{client_secret}") }
+      it 'is blank when there are no credentials' do
+        request = stub :parameters => {}
+        credentials = Credentials.from_params(request)
+        credentials.should be_blank
+      end
+    end
 
-        it 'decodes the credentials' do
-          request = stub :env => { 'HTTP_AUTHORIZATION' => "Basic #{credentials}" }, :parameters => {}
-          credentials = Credentials.from_request(request)
-          credentials.uid.should    == "some-uid"
-          credentials.secret.should == "some-secret"
-        end
+    describe :from_basic do
+      let(:credentials) { Base64.encode64("#{client_id}:#{client_secret}") }
 
-        it 'is blank if Authorization is not Basic' do
-          request = stub :env => { 'HTTP_AUTHORIZATION' => "#{credentials}" }, :parameters => {}
-          credentials = Credentials.from_request(request)
-          credentials.should be_blank
-        end
+      it 'decodes the credentials' do
+        request = stub :env => { 'HTTP_AUTHORIZATION' => "Basic #{credentials}" }
+        credentials = Credentials.from_basic(request)
+        credentials.uid.should    == "some-uid"
+        credentials.secret.should == "some-secret"
+      end
+
+      it 'is blank if Authorization is not Basic' do
+        request = stub :env => { 'HTTP_AUTHORIZATION' => "#{credentials}" }
+        credentials = Credentials.from_basic(request)
+        credentials.should be_blank
+      end
+    end
+
+    describe :from_request do
+      let(:credentials) { Base64.encode64("id-from-header:secret-from-header") }
+
+      let(:request) do
+        stub :env => { 'HTTP_AUTHORIZATION' => "Basic #{credentials}" },
+             :parameters => {
+               :client_id => 'id-from-params',
+               :client_secret => 'secret-from-params'
+             }
+      end
+
+      it 'attempts to find credentials with methods order' do
+        credentials = Credentials.from_request(request, :from_params, :from_basic)
+        credentials.uid.should    == 'id-from-params'
+        credentials.secret.should == 'secret-from-params'
+
+        credentials = Credentials.from_request(request, :from_basic, :from_params)
+        credentials.uid.should    == 'id-from-header'
+        credentials.secret.should == 'secret-from-header'
       end
     end
   end
