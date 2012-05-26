f24fb1455d95e84497a1138025a94443b5d953bf
Felipe Elias Philipp <felipeelias@gmail.com>
2012-05-26 19:29:03 +0200

    Remove isolation

--- lib/doorkeeper/engine.rb
+++ lib/doorkeeper/engine.rb
@@ -1,9 +1,4 @@
 module Doorkeeper
   class Engine < Rails::Engine
-    isolate_namespace Doorkeeper
-
-    config.generators do |g|
-      g.test_framework :rspec, :view_specs => false
-    end
   end
 end
