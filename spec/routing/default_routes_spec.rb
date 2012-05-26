76bdaf33ed3a5dadcd648a331beb0058cec4a978
Felipe Elias Philipp <felipeelias@gmail.com>
2012-05-26 20:57:20 +0200

    Spec for default routing

---
+++ spec/routing/default_routes_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper_integration'
+
+describe 'Default routes' do
+  it 'GET /oauth/authorize routes to authorizations controller' do
+    get('/oauth/authorize').should route_to('doorkeeper/authorizations#new')
+  end
+
+  it 'POST /oauth/authorize routes to authorizations controller' do
+    post('/oauth/authorize').should route_to('doorkeeper/authorizations#create')
+  end
+
+  it 'DELETE /oauth/authorize routes to authorizations controller' do
+    delete('/oauth/authorize').should route_to('doorkeeper/authorizations#destroy')
+  end
+
+  it 'POST /oauth/token routes to tokens controller' do
+    post('/oauth/token').should route_to('doorkeeper/tokens#create')
+  end
+
+  it 'GET /oauth/applications routes to applications controller' do
+    get('/oauth/applications').should route_to('doorkeeper/applications#index')
+  end
+
+  it 'GET /oauth/authorized_applications routes to authorized applications controller' do
+    get('/oauth/authorized_applications').should route_to('doorkeeper/authorized_applications#index')
+  end
+end
