442b680789bae0380b3065c9b840e73f5f7b7fb8
Felipe Elias Philipp <felipeelias@gmail.com>
2012-05-28 01:24:54 +0200

    Update schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(:version => 20120524202412) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "password"
   end
 
