ffd6e58f6ea0cfb6829fe624158f4f6a77d35e4d
Felipe Elias Philipp <felipeelias@gmail.com>
2012-06-02 11:52:46 +0200

    Sample ActionController::Metal controller with doorkeeper_for

---
+++ spec/dummy/app/controllers/metal_controller.rb
@@ -0,0 +1,11 @@
+class MetalController < ActionController::Metal
+  include AbstractController::Callbacks
+  include ActionController::Head
+  include Doorkeeper::Helpers::Filter
+
+  doorkeeper_for :all
+
+  def index
+    self.response_body = { :ok => true }.to_json
+  end
+end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount Doorkeeper::Engine => "/oauth"
+
+  get 'metal.json' => 'metal#index'
   get '/callback', :to => "home#callback"
   get '/sign_in',  :to => "home#sign_in"
   resources :semi_protected_resources
