d1185af52ee02ef31c124568a3310f650a17e032
Felipe Elias Philipp <felipeelias@gmail.com>
2012-06-02 11:52:46 +0200

    Sample ActionController::Metal controller with doorkeeper_for
    From v0.4.1

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'metal.json' => 'metal#index'
   get '/callback', :to => "home#callback"
   get '/sign_in',  :to => "home#sign_in"
   resources :semi_protected_resources
---
+++ spec/requests/protected_resources/metal_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper_integration'
+
+feature 'ActionController::Metal API' do
+  background do
+    @client   = FactoryGirl.create(:application)
+    @resource = User.create!(:name => "Joe", :password => "sekret")
+    @token    = client_is_authorized(@client, @resource)
+  end
+
+  scenario 'client requests protected resource with valid token' do
+    get "/metal.json?access_token=#{@token.token}"
+    should_have_json 'ok', true
+  end
+end
