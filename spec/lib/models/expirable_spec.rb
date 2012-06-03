243184f6b8cf7752fbd67a6b9a95cf88b42f1135
Felipe Elias Philipp <felipeelias@gmail.com>
2012-06-03 13:46:32 +0200

    Remove unused method

--- spec/lib/models/expirable_spec.rb
+++ spec/lib/models/expirable_spec.rb
@@ -30,20 +30,4 @@ describe 'Expirable' do
       subject.should_not be_expired
     end
   end
-
-  describe :time_left do
-    it "returns the time in seconds since it was created" do
-      Timecop.freeze(Time.now) do
-        subject.stub :created_at => Time.now, :expires_in => 10.seconds
-        subject.time_left.should == 10.seconds
-      end
-    end
-
-    it "returns 0 if token has expired" do
-      Timecop.freeze(Time.now + 1.minute) do
-        subject.stub :created_at => 1.minutes.ago, :expires_in => 10.seconds
-        subject.time_left.should == 0
-      end
-    end
-  end
 end
