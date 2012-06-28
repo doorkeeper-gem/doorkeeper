a3987c71795ebb3f9855e6343e81cb44c3731c47
Andrew Cantino & Jeff Moore <pair+andrew+jeff@mavenlink.com>
2012-06-28 14:42:10 -0700

    stop globally messing with forms in the main application

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -1,14 +1,14 @@
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  if html_tag.match("input")
-    message = (error = instance.error_message).respond_to?(:join) ? error.join(",") : error
-    %(<span class="error">
-        #{html_tag}
-        <span class="help-inline">
-          #{message}
-        </span>
-      </span>
-    ).html_safe
-  else
-    html_tag
-  end
-end
+# ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+#   if html_tag.match("input")
+#     message = (error = instance.error_message).respond_to?(:join) ? error.join(",") : error
+#     %(<span class="error">
+#         #{html_tag}
+#         <span class="help-inline">
+#           #{message}
+#         </span>
+#       </span>
+#     ).html_safe
+#   else
+#     html_tag
+#   end
+# end
