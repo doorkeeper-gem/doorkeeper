de6701f6b6c005589838774c6ea655278f67de9f
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2012-07-28 11:25:23 -0700

    Checkpointing.  All tests run green under AR, Mongoid 2.4.x.  Only failing on unique index violations on Mongoid 3.0.x

--- lib/doorkeeper/models/access_token.rb
+++ lib/doorkeeper/models/access_token.rb
@@ -6,7 +6,7 @@ module Doorkeeper
     include Doorkeeper::Models::Accessible
     include Doorkeeper::Models::Scopes
 
-    belongs_to :application, :class_name => "Doorkeeper::Application"
+    belongs_to :application, :class_name => "Doorkeeper::Application", :inverse_of => :access_tokens
 
     validates :application_id, :token, :presence => true
     validates :token, :uniqueness => true
