5c492fc8203784d35757796fc2ab1f07e24cf8d1
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2012-07-28 12:57:47 -0700

    Get unique index tests passing - need to adjust database, rspec config

--- spec/support/orm/mongoid.rb
+++ spec/support/orm/mongoid.rb
@@ -2,7 +2,7 @@ DatabaseCleaner[:mongoid].strategy = :truncation
 DatabaseCleaner[:mongoid].clean_with :truncation
 
 RSpec.configure do |config|
-  config.before :suite do
+  config.before do
     Doorkeeper::Application.create_indexes
     Doorkeeper::AccessGrant.create_indexes
     Doorkeeper::AccessToken.create_indexes
