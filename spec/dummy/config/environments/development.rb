661ea1212b74011dde25d44bc1d6a252a60f95a2
Kazuya NUMATA <numata@gmail.com>
2013-03-05 15:44:16 +0900

    Set config.eager_load in config/environments/* to support Rails 4.0
    This eliminates the warnings

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -27,4 +27,6 @@ Dummy::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.eager_load = false
 end
--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -36,4 +36,6 @@ Dummy::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.eager_load = false
 end
