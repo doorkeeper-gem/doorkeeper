0fc7336f1bb702bd37a4b72dd5a9b7087299f71a
Akira Matsuda <ronnie@dio.jp>
2013-06-19 19:43:09 +0900

    "warning: `&' interpreted as argument prefix"

--- lib/doorkeeper/helpers/controller.rb
+++ lib/doorkeeper/helpers/controller.rb
@@ -15,15 +15,15 @@ module Doorkeeper
       end
 
       def current_resource_owner
-        instance_eval &Doorkeeper.configuration.authenticate_resource_owner
+        instance_eval(&Doorkeeper.configuration.authenticate_resource_owner)
       end
 
       def resource_owner_from_credentials
-        instance_eval &Doorkeeper.configuration.resource_owner_from_credentials
+        instance_eval(&Doorkeeper.configuration.resource_owner_from_credentials)
       end
 
       def authenticate_admin!
-        instance_eval &Doorkeeper.configuration.authenticate_admin
+        instance_eval(&Doorkeeper.configuration.authenticate_admin)
       end
 
       def server
