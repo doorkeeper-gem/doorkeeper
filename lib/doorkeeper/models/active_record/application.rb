f6ef51d6ddb0f59c8d62503506ae68c8e4940181
Tute Costa <tutecosta@gmail.com>
2013-07-16 14:59:09 +0200

    Trailing white space and Ruby 1.9 hash syntax

--- lib/doorkeeper/models/active_record/application.rb
+++ lib/doorkeeper/models/active_record/application.rb
@@ -3,20 +3,20 @@ module Doorkeeper
     self.table_name = :oauth_applications
 
     if ActiveRecord::VERSION::MAJOR >= 4
-      has_many :authorized_tokens, -> { where(revoked_at: nil) }, :class_name => "AccessToken"
+      has_many :authorized_tokens, -> { where(revoked_at: nil) }, class_name: "AccessToken"
     else
-      has_many :authorized_tokens, :class_name => "AccessToken", :conditions => { :revoked_at => nil }      
+      has_many :authorized_tokens, class_name: "AccessToken", conditions: { revoked_at: nil }
     end
-    has_many :authorized_applications, :through => :authorized_tokens, :source => :application
+    has_many :authorized_applications, through: :authorized_tokens, source: :application
 
     def self.column_names_with_table
       self.column_names.map { |c| "#{self.table_name}.#{c}" }
     end
 
     def self.authorized_for(resource_owner)
-      joins(:authorized_applications).
-        where(:oauth_access_tokens => { :resource_owner_id => resource_owner.id, :revoked_at => nil }).
-        group(column_names_with_table.join(','))
+      joins(:authorized_applications)
+        .where(oauth_access_tokens: { resource_owner_id: resource_owner.id, revoked_at: nil })
+        .group(column_names_with_table.join(','))
     end
   end
 end
