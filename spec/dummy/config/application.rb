4b7e0f637384ec646b294fa7b83b5413be542d4b
Tute Costa <tutecosta@gmail.com>
2013-12-26 14:18:07 -0300

    require in favor of autoload
    See
    http://tenderlovemaking.com/2012/06/18/removing-config-threadsafe.html

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -20,9 +20,6 @@ module Dummy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
