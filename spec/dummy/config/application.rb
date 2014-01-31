84f25f59d0f1a100a0132679c0acfccf61930b34
jasl <jasl9187@hotmail.com>
2014-02-01 01:03:20 +0800

    check i18n can respond enforce_available_locales

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -51,7 +51,7 @@ module Dummy
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    I18n.enforce_available_locales = false
+    I18n.enforce_available_locales = false if I18n.respond_to? :enforce_available_locales
   end
 end
 
