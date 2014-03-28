68b4da5039db71541782bf8708f73ac38ca98924
Martin Lagrange <martin@ifeelgoods.com>
2014-03-28 18:43:12 +0100

    Add revoke route

--- spec/routing/default_routes_spec.rb
+++ spec/routing/default_routes_spec.rb
@@ -17,6 +17,10 @@ describe 'Default routes' do
     expect(post('/oauth/token')).to route_to('doorkeeper/tokens#create')
   end
 
+  it 'POST /oauth/revoke routes to tokens controller' do
+    post('/oauth/revoke').should route_to('doorkeeper/tokens#revoke')
+  end
+
   it 'GET /oauth/applications routes to applications controller' do
     expect(get('/oauth/applications')).to route_to('doorkeeper/applications#index')
   end
