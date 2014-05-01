d5c88159770bb0e7d664afdd060301df8af13e46
Kenn Ejima <kenn.ejima@gmail.com>
2014-04-30 17:45:22 -0700

    Remove unnecessary line

--- lib/doorkeeper/oauth/helpers/find_or_create_access_token.rb
+++ lib/doorkeeper/oauth/helpers/find_or_create_access_token.rb
@@ -4,7 +4,6 @@ module Doorkeeper
       module FindOrCreateAccessToken
         def self.for(application, resource_owner_id, scopes, expires_in, use_refresh_token)
           if Doorkeeper.configuration.reuse_access_token
-            token = Doorkeeper::AccessToken.send :last_authorized_token_for, application, resource_owner_id
             access_token = Doorkeeper::AccessToken.matching_token_for(application, resource_owner_id, scopes)
             if access_token && !access_token.expired?
               return access_token
