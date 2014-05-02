3cbfe54bcaefbef4682e46e63a233edf74cd8fe0
Kenn Ejima <kenn.ejima@gmail.com>
2014-05-02 15:07:53 -0700

    Fix the wrong application of rebase.

--- spec/lib/oauth/password_access_token_request_spec.rb
+++ spec/lib/oauth/password_access_token_request_spec.rb
@@ -2,33 +2,33 @@ require 'spec_helper_integration'
 
 module Doorkeeper::OAuth
   describe PasswordAccessTokenRequest do
-    let(:server) { double :server, :default_scopes => Doorkeeper::OAuth::Scopes.new, :access_token_expires_in => 2.hours, :refresh_token_enabled? => false }
+    let(:server) { double :server, default_scopes: Doorkeeper::OAuth::Scopes.new, access_token_expires_in: 2.hours, refresh_token_enabled?: false }
     let(:credentials) { Client::Credentials.new(client.uid, client.secret) }
     let(:client) { FactoryGirl.create(:application) }
-    let(:owner)  { double :owner, :id => 99 }
+    let(:owner)  { double :owner, id: 99 }
 
     subject do
       PasswordAccessTokenRequest.new(server, credentials, owner)
     end
 
     it 'issues a new token for the client' do
-      expect {
+      expect do
         subject.authorize
-      }.to change { client.access_tokens.count }.by(1)
+      end.to change { client.access_tokens.count }.by(1)
     end
 
     it 'issues a new token without a client' do
-      expect {
+      expect do
         subject.credentials = nil
         subject.authorize
-      }.to change { Doorkeeper::AccessToken.count }.by(1)
+      end.to change { Doorkeeper::AccessToken.count }.by(1)
     end
 
     it 'does not issue a new token with an invalid client' do
-      expect {
+      expect do
         subject.client = nil
         subject.authorize
-      }.to_not change { Doorkeeper::AccessToken.count }
+      end.to_not change { Doorkeeper::AccessToken.count }
 
       expect(subject.error).to eq(:invalid_client)
     end
@@ -45,7 +45,7 @@ module Doorkeeper::OAuth
     end
 
     it 'creates token even when there is already one (default)' do
-      FactoryGirl.create(:access_token, :application_id => client.id, :resource_owner_id => owner.id)
+      FactoryGirl.create(:access_token, application_id: client.id, resource_owner_id: owner.id)
       expect do
         subject.authorize
       end.to change { Doorkeeper::AccessToken.count }.by(1)
@@ -53,7 +53,7 @@ module Doorkeeper::OAuth
 
     it 'skips token creation if there is already one' do
       Doorkeeper.configuration.stub(:reuse_access_token).and_return(true)
-      FactoryGirl.create(:access_token, :application_id => client.id, :resource_owner_id => owner.id)
+      FactoryGirl.create(:access_token, application_id: client.id, resource_owner_id: owner.id)
       expect do
         subject.authorize
       end.to_not change { Doorkeeper::AccessToken.count }
@@ -61,7 +61,7 @@ module Doorkeeper::OAuth
 
     describe "with scopes" do
       subject do
-        PasswordAccessTokenRequest.new(server, client, owner, :scope => 'public')
+        PasswordAccessTokenRequest.new(server, client, owner, scope: 'public')
       end
 
       it 'validates the current scope' do
@@ -71,10 +71,10 @@ module Doorkeeper::OAuth
       end
 
       it 'creates the token with scopes' do
-        allow(server).to receive(:scopes).and_return(Doorkeeper::OAuth::Scopes.from_string("public"))
-        expect {
+        allow(server).to receive(:scopes).and_return(Doorkeeper::OAuth::Scopes.from_string('public'))
+        expect do
           subject.authorize
-        }.to change { Doorkeeper::AccessToken.count }.by(1)
+        end.to change { Doorkeeper::AccessToken.count }.by(1)
         expect(Doorkeeper::AccessToken.last.scopes).to include('public')
       end
     end
