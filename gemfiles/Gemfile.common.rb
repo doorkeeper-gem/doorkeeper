698c2a10986cac8d72bac62de6f58704ec610d36
Tute Costa <tutecosta@gmail.com>
2014-11-09 10:59:57 -0500

    Add Rails 4.2 to Travis CI

--- gemfiles/Gemfile.common.rb
+++ gemfiles/Gemfile.common.rb
@@ -1,8 +1,8 @@
-ENV['rails'] ||= '4.1'
+ENV['rails'] ||= '4.2.0.beta4'
 
 source 'https://rubygems.org'
 
-gem 'rails', "~> #{ENV['rails']}.0"
+gem 'rails', "~> #{ENV['rails']}"
 
 if ENV['rails'][0] == '4'
   gem 'database_cleaner'
