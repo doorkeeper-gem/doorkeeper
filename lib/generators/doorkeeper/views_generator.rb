289d68a52c41929ee9b1aab89e7fc7d223037eec
jasl <jasl9187@hotmail.com>
2014-11-17 10:53:36 +0800

    dump layouts when generating views

--- lib/generators/doorkeeper/views_generator.rb
+++ lib/generators/doorkeeper/views_generator.rb
@@ -1,14 +1,13 @@
 module Doorkeeper
   module Generators
     class ViewsGenerator < ::Rails::Generators::Base
-      source_root File.expand_path('../../../../app/views/doorkeeper', __FILE__)
+      source_root File.expand_path('../../../../app/views', __FILE__)
 
-      desc 'Copies default Doorkeeper views to your application.'
+      desc 'Copies default Doorkeeper views and layouts to your application.'
 
       def manifest
-        directory 'applications', 'app/views/doorkeeper/applications'
-        directory 'authorizations', 'app/views/doorkeeper/authorizations'
-        directory 'authorized_applications', 'app/views/doorkeeper/authorized_applications'
+        directory 'doorkeeper', 'app/views/doorkeeper'
+        directory 'layouts/doorkeeper', 'app/views/layouts/doorkeeper'
       end
     end
   end
