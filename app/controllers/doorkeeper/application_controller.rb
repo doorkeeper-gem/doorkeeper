c6ab353808384cde7115a1743e088a390572b8dd
Phillip Baker <phillip@digitalocean.com>
2014-12-08 21:49:46 -0500

    Use Doorkeeper::ApplicationController.protect_from_forgery
    Since the Doorkeeper controllers inherit from Doorkeeper::Application
    (which inherits directly from ActionController::Base) and not
    ApplicationController, they never call `protect_from_forgery`, which
    means that non-GET methods don’t validate CSRF tokens. Thus, it’s
    possible for an attacker to host a form on an arbitrary URL, and if a
    users is logged into a site that uses Doorkeeper visits the URL, the
    attacker can grant access to a application on that site.
    
    Conflicts:
    	app/controllers/doorkeeper/application_controller.rb

--- app/controllers/doorkeeper/application_controller.rb
+++ app/controllers/doorkeeper/application_controller.rb
@@ -3,5 +3,11 @@ module Doorkeeper
     include Helpers::Controller
 
     helper 'doorkeeper/form_errors'
+
+    if ::Rails.version.to_i < 4
+      protect_from_forgery
+    else
+      protect_from_forgery with: :exception
+    end
   end
 end
