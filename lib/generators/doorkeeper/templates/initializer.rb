421a191cc0aa2a72c749dff8eb1eb654faffe5dc
Tute Costa <tutecosta@gmail.com>
2015-01-23 21:05:48 -0500

    typo fix
    Related with #560
    https://github.com/doorkeeper-gem/doorkeeper/pull/560
    
    [ci skip]

--- lib/generators/doorkeeper/templates/initializer.rb
+++ lib/generators/doorkeeper/templates/initializer.rb
@@ -95,7 +95,7 @@ Doorkeeper.configure do
 
   # Under some circumstances you might want to have applications auto-approved,
   # so that the user skips the authorization step.
-  # For example if dealing with trusted a application.
+  # For example if dealing with a trusted application.
   # skip_authorization do |resource_owner, client|
   #   client.superapp? or resource_owner.admin?
   # end
