ded4275fc3c6795272ab7ad4e2fdc4ea5fa2ca4c
Ross Sharrott <rsharrott@moneytree.jp>
2015-02-03 14:32:17 +0900

    repect the expire in time even with the revoke on use code

--- lib/doorkeeper/models/concerns/revocable.rb
+++ lib/doorkeeper/models/concerns/revocable.rb
@@ -2,7 +2,9 @@ module Doorkeeper
   module Models
     module Revocable
       def revoke(clock = DateTime)
+        Doorkeeper.configuration.refresh_token
         update_attribute :revoked_at, clock.now
+        Doorkeeper.configuration.refresh_token_revoked_on_use
       end
 
       def revoke_in(time)
@@ -13,12 +15,22 @@ module Doorkeeper
         !!(revoked_at && revoked_at <= DateTime.now)
       end
 
+      def previous_active_refresh_token
+        previous_refresh_token = AccessToken.by_refresh_token(previous_refresh_token) unless previous_refresh_token.nil?
+        return nil if previous_refresh_token.nil? or previous_refresh_token.revoked?
+        previous_refresh_token
+      end
+
       def revoke_previous_refresh_token!
-        unless previous_refresh_token.nil?
-          old_refresh_token = AccessToken.by_refresh_token(previous_refresh_token)
-          old_refresh_token.revoke unless old_refresh_token.nil? or old_refresh_token.revoked_at
-          update_attribute :previous_refresh_token, nil
+        old_refresh_token =　previous_active_refresh_token
+        if old_refresh_token
+          if Doorkeeper.configuration.refresh_token_revoked_in
+            old_refresh_token.revoke
+          else
+            old_refresh_token.revoke_in(Doorkeeper.configuration.refresh_token_revoked_in)
+          end
         end
+        update_attribute :previous_refresh_token, nil
       end
     end
   end
