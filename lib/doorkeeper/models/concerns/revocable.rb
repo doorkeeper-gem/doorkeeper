12cbc28c0a9bb8de3e7cfc58ad770e1e3d93278c
Ross Sharrott <rsharrott@moneytree.jp>
2015-02-03 16:20:02 +0900

    updates to meet tests

--- lib/doorkeeper/models/concerns/revocable.rb
+++ lib/doorkeeper/models/concerns/revocable.rb
@@ -2,7 +2,6 @@ module Doorkeeper
   module Models
     module Revocable
       def revoke(clock = DateTime)
-        Doorkeeper.configuration.refresh_token
         update_attribute :revoked_at, clock.now
         Doorkeeper.configuration.refresh_token_revoked_on_use
       end
@@ -15,14 +14,10 @@ module Doorkeeper
         !!(revoked_at && revoked_at <= DateTime.now)
       end
 
-      def previous_active_refresh_token
-        previous_refresh_token = AccessToken.by_refresh_token(previous_refresh_token) unless previous_refresh_token.nil?
-        return nil if previous_refresh_token.nil? or previous_refresh_token.revoked?
-        previous_refresh_token
-      end
-
       def revoke_previous_refresh_token!
-        old_refresh_token =　previous_active_refresh_token
+        return if previous_refresh_token.nil?
+        old_refresh_token = AccessToken.by_refresh_token(previous_refresh_token)
+        old_refresh_token = nil if old_refresh_token.nil? or old_refresh_token.revoked?
         if old_refresh_token
           if Doorkeeper.configuration.refresh_token_revoked_in
             old_refresh_token.revoke
