db9c00f68c8d921b2f0c9997be0ab2c05d830ef0
Tute Costa <tutecosta@gmail.com>
2015-02-20 14:34:00 -0500

    Refactor Doorkeeper::Rails::Helpers
    Makes explicit what the public API is to inject doorkeeper behavior into
    application endpoints.
    
    This should be the only interface between Rails and doorkeeper. Other
    frameworks can define their own interface and use doorkeeper too. This
    is a step into decoupling doorkeeper from Rails, so that integration
    is easier.
    
    Related with https://github.com/doorkeeper-gem/doorkeeper/pull/567.

--- lib/doorkeeper/rails/helpers.rb
+++ lib/doorkeeper/rails/helpers.rb
@@ -3,41 +3,11 @@ module Doorkeeper
     module Helpers
       extend ActiveSupport::Concern
 
-      module ClassMethods
-        def doorkeeper_for(*args, &block)
-          fail Errors::DoorkeeperError, "`doorkeeper_for` no longer available", <<-eos
-\nStarting in version 2.0.0 of doorkeeper gem, `doorkeeper_for` is no longer
-available. Please change `doorkeeper_for` calls in your application with:
-
-  before_action :doorkeeper_authorize!
-
-For more information check the README:
-https://github.com/doorkeeper-gem/doorkeeper#protecting-resources-with-oauth-aka-your-api-endpoint\n
-          eos
-        end
-      end
-
-      def doorkeeper_token
-        @_doorkeeper_token ||= OAuth::Token.authenticate request, *Doorkeeper.configuration.access_token_methods
-      end
-
-      def valid_doorkeeper_token?(*scopes)
-        doorkeeper_token && doorkeeper_token.acceptable?(scopes)
-      end
-
       def doorkeeper_authorize!(*scopes)
-        scopes ||= Doorkeeper.configuration.default_scopes
+        @_doorkeeper_scopes = scopes || Doorkeeper.configuration.default_scopes
 
-        unless valid_doorkeeper_token?(*scopes)
-          if !doorkeeper_token || !doorkeeper_token.accessible?
-            error = OAuth::InvalidTokenResponse.from_access_token(doorkeeper_token)
-            options = doorkeeper_unauthorized_render_options
-          else
-            error = OAuth::ForbiddenTokenResponse.from_scopes(scopes)
-            options = doorkeeper_forbidden_render_options
-          end
-          headers.merge!(error.headers.reject { |k| ['Content-Type'].include? k })
-          doorkeeper_error_renderer(error, options)
+        if doorkeeper_token_is_invalid?
+          doorkeeper_render_error
         end
       end
 
@@ -49,13 +19,71 @@ https://github.com/doorkeeper-gem/doorkeeper#protecting-resources-with-oauth-aka
         nil
       end
 
-      def doorkeeper_error_renderer(error, options = {})
+      private
+
+      def doorkeeper_token_is_invalid?
+        !doorkeeper_token || !doorkeeper_token.acceptable?(@_doorkeeper_scopes)
+      end
+
+      def doorkeeper_render_error
+        error = doorkeeper_error
+        headers.merge! error.headers.reject { |k| "Content-Type" == k }
+        doorkeeper_render_error_with(error)
+      end
+
+      def doorkeeper_render_error_with(error)
+        options = doorkeeper_render_options || {}
         if options.blank?
           head error.status
         else
           options[:status] = error.status
           options[:layout] = false if options[:layout].nil?
           render options
+        end
+      end
+
+      def doorkeeper_error
+        if doorkeeper_invalid_token_response?
+          OAuth::InvalidTokenResponse.from_access_token(doorkeeper_token)
+        else
+          OAuth::ForbiddenTokenResponse.from_scopes(@_doorkeeper_scopes)
+        end
+      end
+
+      def doorkeeper_render_options
+        if doorkeeper_invalid_token_response?
+          doorkeeper_unauthorized_render_options
+        else
+          doorkeeper_forbidden_render_options
+        end
+      end
+
+      def doorkeeper_invalid_token_response?
+        !doorkeeper_token || !doorkeeper_token.accessible?
+      end
+
+      def doorkeeper_token
+        @_doorkeeper_token ||= OAuth::Token.authenticate(
+          request,
+          *Doorkeeper.configuration.access_token_methods
+        )
+      end
+
+      module ClassMethods
+        def doorkeeper_for(*_args)
+          fail(
+            Errors::DoorkeeperError,
+            "`doorkeeper_for` no longer available",
+            <<-eos
+\nStarting in version 2.0.0 of doorkeeper gem, `doorkeeper_for` is no longer
+available. Please change `doorkeeper_for` calls in your application with:
+
+  before_action :doorkeeper_authorize!
+
+For more information check the README:
+https://github.com/doorkeeper-gem/doorkeeper#protecting-resources-with-oauth-aka-your-api-endpoint\n
+eos
+          )
         end
       end
     end
