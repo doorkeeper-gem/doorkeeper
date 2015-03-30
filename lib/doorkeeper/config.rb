d7b1e994b9ecc05d2992f7c173daa5cb466e6918
Maxim Küpper <maxim.kuepper@i22.de>
2015-03-30 13:00:18 +0200

    adds initializers for developments uris

--- lib/doorkeeper/config.rb
+++ lib/doorkeeper/config.rb
@@ -194,6 +194,7 @@ and that your `initialize_models!` method doesn't raise any errors.\n
     option :authorization_code_expires_in,  default: 600
     option :orm,                            default: :active_record
     option :native_redirect_uri,            default: 'urn:ietf:wg:oauth:2.0:oob'
+    option :development_uris,               default: :disabled
     option :active_record_options,          default: {}
     option :realm,                          default: 'Doorkeeper'
     option :force_ssl_in_redirect_uri,      default: !Rails.env.development?
