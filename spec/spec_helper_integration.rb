b28c70e4bad0b3307262f9c56a571129fa66b94f
Tute Costa <tutecosta@gmail.com>
2015-05-04 22:09:55 -0400

    Remove ORM specifics from spec

--- spec/spec_helper_integration.rb
+++ spec/spec_helper_integration.rb
@@ -22,11 +22,7 @@ end
 Rails.logger.info "====> Rails version: #{Rails.version}"
 Rails.logger.info "====> Ruby version: #{RUBY_VERSION}"
 
-if [:mongoid2, :mongoid3, :mongoid4].include?(DOORKEEPER_ORM)
-  require "support/orm/mongoid"
-else
-  require "support/orm/#{DOORKEEPER_ORM}"
-end
+require "support/orm/#{DOORKEEPER_ORM}"
 
 ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
 
