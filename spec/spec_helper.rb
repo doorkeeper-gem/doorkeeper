1f19ab89fc53d7e865d584d103b1047481edf343
Tobias Bielohlawek <t.bielohlawek@web.de>
2015-08-21 00:42:24 +0200

    Fix rspec warnings, require env in spec_helper.rb
    Spec threw warnings as raise_error didn’t provide an Error Class to assert on. Indeed in one case the wrong error was raised than expected. In addition, requiring the app env in spec_helper allows to run single test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '../lib'))
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '../app'))
+
+require 'capybara/rspec'
+require 'dummy/config/environment'
