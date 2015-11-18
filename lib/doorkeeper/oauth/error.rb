3bacfe14ce80cddac8c682ac375ea17a368bc919
Ryan Schlesinger <ryan@outstand.com>
2015-11-18 13:36:29 -0800

    Specify default error translation key

--- lib/doorkeeper/oauth/error.rb
+++ lib/doorkeeper/oauth/error.rb
@@ -2,7 +2,9 @@ module Doorkeeper
   module OAuth
     class Error < Struct.new(:name, :state)
       def description
-        I18n.translate name, scope: [:doorkeeper, :errors, :messages]
+        I18n.translate name,
+          scope: [:doorkeeper, :errors, :messages],
+          default: :server_error
       end
     end
   end
--- spec/lib/oauth/error_spec.rb
+++ spec/lib/oauth/error_spec.rb
@@ -4,15 +4,19 @@ require 'doorkeeper/oauth/error'
 
 module Doorkeeper::OAuth
   describe Error do
-    subject { Error.new(:some_error, :some_state) }
+    subject(:error) { Error.new(:some_error, :some_state) }
 
     it { expect(subject).to respond_to(:name) }
     it { expect(subject).to respond_to(:state) }
 
     describe :description do
       it 'is translated from translation messages' do
-        expect(I18n).to receive(:translate).with(:some_error, scope: [:doorkeeper, :errors, :messages])
-        subject.description
+        expect(I18n).to receive(:translate).with(
+          :some_error,
+          scope: [:doorkeeper, :errors, :messages],
+          default: :server_error
+        )
+        error.description
       end
     end
   end
