50ea958a4bc8138ccf171a8959010afb7986595d
Ryan Schlesinger <ryan@outstand.com>
2015-11-18 13:41:04 -0800

    Make translate call clearer

--- lib/doorkeeper/oauth/error.rb
+++ lib/doorkeeper/oauth/error.rb
@@ -2,9 +2,11 @@ module Doorkeeper
   module OAuth
     class Error < Struct.new(:name, :state)
       def description
-        I18n.translate name,
+        I18n.translate(
+          name,
           scope: [:doorkeeper, :errors, :messages],
           default: :server_error
+        )
       end
     end
   end
