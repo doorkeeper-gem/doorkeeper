8e961328776b56f921790a8f096816e8e5416011
xendoc <xendoc@users.noreply.github.com>
2016-05-27 12:19:06 +0900

    Remove not null constraint
    The constraint is for application_id in oauth_access_tokens

--- lib/generators/doorkeeper/templates/migration.rb
+++ lib/generators/doorkeeper/templates/migration.rb
@@ -31,7 +31,7 @@ class CreateDoorkeeperTables < ActiveRecord::Migration
 
     create_table :oauth_access_tokens do |t|
       t.integer  :resource_owner_id
-      t.references :application,       null: false
+      t.references :application
 
       # If you use a custom token generator you may need to change this column
       # from string to text, so that it accepts tokens larger than 255
