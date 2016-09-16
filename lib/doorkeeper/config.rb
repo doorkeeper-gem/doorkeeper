0d8a6220ee3e9b155d9a5a272d604cd6ecb59db4
Bruce Park <brucepark0@gmail.com>
2016-09-16 16:03:14 -0700

    Add documentation
    - Document MissingConfiguration Error
    - Document Config::Builder class
    - Addresses #859

--- lib/doorkeeper/config.rb
+++ lib/doorkeeper/config.rb
@@ -1,5 +1,7 @@
 module Doorkeeper
   class MissingConfiguration < StandardError
+    # Defines a MissingConfiguration error for a missing Doorkeeper
+    # configuration
     def initialize
       super('Configuration for doorkeeper missing. Do you have doorkeeper initializer?')
     end
@@ -47,6 +49,10 @@ doorkeeper.
         @config
       end
 
+      # Provide support for an owner to be assigned to each registered
+      # application (disabled by default)
+      # Optional parameter confirmation: true (default false) if you want
+      # to enforce ownership of a registered application
       def enable_application_owner(opts = {})
         @config.instance_variable_set('@enable_application_owner', true)
         confirm_application_owner if opts[:confirmation].present? && opts[:confirmation]
@@ -56,6 +62,7 @@ doorkeeper.
         @config.instance_variable_set('@confirm_application_owner', true)
       end
 
+      # Define access token scopes for your provider
       def default_scopes(*scopes)
         @config.instance_variable_set('@default_scopes', OAuth::Scopes.from_array(scopes))
       end
@@ -64,36 +71,58 @@ doorkeeper.
         @config.instance_variable_set('@optional_scopes', OAuth::Scopes.from_array(scopes))
       end
 
+      # Change the way client credentials are retrieved from the request object.
+      # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
+      # falls back to the `:client_id` and `:client_secret` params from the
+      # `params` object.
       def client_credentials(*methods)
         @config.instance_variable_set('@client_credentials', methods)
       end
 
+      # Change the way access token is authenticated from the request object.
+      # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
+      # falls back to the `:access_token` or `:bearer_token` params from the
+      # `params` object.
       def access_token_methods(*methods)
         @config.instance_variable_set('@access_token_methods', methods)
       end
 
+      # Issue access tokens with refresh token (disabled by default)
       def use_refresh_token
         @config.instance_variable_set('@refresh_token_enabled', true)
       end
 
+      # WWW-Authenticate Realm (default "Doorkeeper").
       def realm(realm)
         @config.instance_variable_set('@realm', realm)
       end
 
+      # Reuse access token for the same resource owner within an application
+      # (disabled by default)
+      # Rationale: https://github.com/doorkeeper-gem/doorkeeper/issues/383
       def reuse_access_token
         @config.instance_variable_set("@reuse_access_token", true)
       end
 
+      # Forces the usage of the HTTPS protocol in non-native redirect uris
+      # (enabled by default in non-development environments). OAuth2
+      # delegates security in communication to the HTTPS protocol so it is
+      # wise to keep this enabled.
       def force_ssl_in_redirect_uri(boolean)
         @config.instance_variable_set("@force_ssl_in_redirect_uri", boolean)
       end
 
+      # Use a custom class for generating the access token.
+      # https://github.com/doorkeeper-gem/doorkeeper#custom-access-token-generator
       def access_token_generator(access_token_generator)
         @config.instance_variable_set(
           '@access_token_generator', access_token_generator
         )
       end
 
+      # The controller Doorkeeper::ApplicationController inherits from.
+      # Defaults to ActionController::Base.
+      # https://github.com/doorkeeper-gem/doorkeeper#custom-base-controller
       def base_controller(base_controller)
         @config.instance_variable_set('@base_controller', base_controller)
       end
