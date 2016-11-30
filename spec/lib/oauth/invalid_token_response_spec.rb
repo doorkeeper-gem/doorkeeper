f0cc04a9a8e1a478ad21aa0d1f271b480ce96423
Rodrigo Pinto <rodrigo.pinto@askcharlie.com>
2016-11-30 16:25:55 +0100

    Fix hound ci style violation. :cop:

--- spec/lib/oauth/invalid_token_response_spec.rb
+++ spec/lib/oauth/invalid_token_response_spec.rb
@@ -5,49 +5,49 @@ require 'doorkeeper/oauth/invalid_token_response'
 
 module Doorkeeper::OAuth
   describe InvalidTokenResponse do
-    describe '#name' do
+    describe "#name" do
       it  { expect(subject.name).to eq(:invalid_token) }
     end
 
-    describe '#status' do
+    describe "#status" do
       it { expect(subject.status).to eq(:unauthorized) }
     end
 
     describe :from_access_token do
       let(:response) { InvalidTokenResponse.from_access_token(access_token) }
 
-      context 'revoked' do
+      context "revoked" do
         let(:access_token) { double(revoked?: true, expired?: true) }
 
-        it 'sets a description' do
-          expect(response.description).to include('revoked')
+        it "sets a description" do
+          expect(response.description).to include("revoked")
         end
 
-        it 'sets the reason' do
+        it "sets the reason" do
           expect(response.reason).to eq(:revoked)
         end
       end
 
-      context 'expired' do
+      context "expired" do
         let(:access_token) { double(revoked?: false, expired?: true) }
 
-        it 'sets a description' do
-          expect(response.description).to include('expired')
+        it "sets a description" do
+          expect(response.description).to include("expired")
         end
 
-        it 'sets the reason' do
+        it "sets the reason" do
           expect(response.reason).to eq(:expired)
         end
       end
 
-      context 'unkown' do
+      context "unkown" do
         let(:access_token) { double(revoked?: false, expired?: false) }
 
-        it 'sets a description' do
-          expect(response.description).to include('invalid')
+        it "sets a description" do
+          expect(response.description).to include("invalid")
         end
 
-        it 'sets the reason' do
+        it "sets the reason" do
           expect(response.reason).to eq(:unknown)
         end
       end
