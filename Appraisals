80164d104f0af40a7fd51682329acb5193d33f9b
Jon Moss <me@jonathanmoss.me>
2017-01-16 13:36:21 -0500

    Switch to Appraisal + specs passing on Rails master

---
+++ Appraisals
@@ -0,0 +1,14 @@
+appraise "rails-4-2" do
+  gem "rails", "~> 4.2.0"
+end
+
+appraise "rails-5-0" do
+  gem "rails", "~> 5.0.0"
+  gem "rspec-rails", "~> 3.5"
+end
+
+appraise "rails-5-1" do
+  gem "rails", github: "rails/rails"
+  gem "arel", github: "rails/arel"
+  gem "rspec-rails", "~> 3.5"
+end
--- spec/support/http_method_shim.rb
+++ spec/support/http_method_shim.rb
@@ -2,23 +2,37 @@
 # in favor of keyword arguments. However, the keyword argument form is only
 # supported in Rails 5+. Since we support back to 4, we need some sort of shim
 # to avoid super noisy deprecations when running tests.
-module HTTPMethodShim
-  def get(path, params = nil, headers = nil)
+module RoutingHTTPMethodShim
+  def get(path, params = {}, headers = nil)
     super(path, params: params, headers: headers)
   end
 
-  def post(path, params = nil, headers = nil)
+  def post(path, params = {}, headers = nil)
     super(path, params: params, headers: headers)
   end
 
-  def put(path, params = nil, headers = nil)
+  def put(path, params = {}, headers = nil)
     super(path, params: params, headers: headers)
+  end
+end
+
+module ControllerHTTPMethodShim
+  def get(path, params = {})
+    super(path, params: params)
+  end
+
+  def post(path, params = {})
+    super(path, params: params)
+  end
+
+  def put(path, params = {})
+    super(path, params: params)
   end
 end
 
 if ::Rails::VERSION::MAJOR >= 5
   RSpec.configure do |config|
-    config.include HTTPMethodShim, type: :controller
-    config.include HTTPMethodShim, type: :request
+    config.include ControllerHTTPMethodShim, type: :controller
+    config.include RoutingHTTPMethodShim, type: :request
   end
 end
