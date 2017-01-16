1a65ee878825f3a89e0979219bd4aa1419ec1105
Jon Moss <me@jonathanmoss.me>
2017-01-16 14:15:41 -0500

    Add `:doc:` to private methods that are Public API
    [ci skip]

--- lib/doorkeeper/helpers/controller.rb
+++ lib/doorkeeper/helpers/controller.rb
@@ -5,11 +5,11 @@ module Doorkeeper
     module Controller
       private
 
-      def authenticate_resource_owner!
+      def authenticate_resource_owner! # :doc:
         current_resource_owner
       end
 
-      def current_resource_owner
+      def current_resource_owner # :doc:
         instance_eval(&Doorkeeper.configuration.authenticate_resource_owner)
       end
 
@@ -17,7 +17,7 @@ module Doorkeeper
         instance_eval(&Doorkeeper.configuration.resource_owner_from_credentials)
       end
 
-      def authenticate_admin!
+      def authenticate_admin! # :doc:
         instance_eval(&Doorkeeper.configuration.authenticate_admin)
       end
 
@@ -25,7 +25,7 @@ module Doorkeeper
         @server ||= Server.new(self)
       end
 
-      def doorkeeper_token
+      def doorkeeper_token # :doc:
         @token ||= OAuth::Token.authenticate request, *config_methods
       end
 
