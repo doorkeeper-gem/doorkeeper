9170e5b48565add2e5f85c48344a7986bd39a54e
Nikita Bulaj <bulajnikita@gmail.com>
2018-02-04 00:34:39 +0300

    Fix #913: Deferred ORM (ActiveRecord) models loading

--- lib/doorkeeper/version.rb
+++ lib/doorkeeper/version.rb
@@ -1,3 +1,15 @@
 module Doorkeeper
-  VERSION = "4.2.6".freeze
+  def self.gem_version
+    Gem::Version.new VERSION::STRING
+  end
+
+  module VERSION
+    # Semantic versioning
+    MAJOR = 4
+    MINOR = 2
+    TINY = 6
+
+    # Full version number
+    STRING = [MAJOR, MINOR, TINY].compact.join('.')
+  end
 end
