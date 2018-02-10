c5ecad21ef3697fe87bd02dfb56180edcb710e92
Justin Bull <justin@wealthsimple.com>
2018-02-10 13:47:16 -0500

    Allow public clients to authenticate without client_secret
    Add Application#confidential
    
    Add dummy migration for Application#confidential
    
    Because Dummy app is now Rails 5.1, the old migrations' ancestor class needed to be explicitly the 4.2 variety.
    
    Expose app confidentiality in views & controllers
    
    Allow public applications to be found if secret is blank
    
    Don't use #client_via_uid fallback on Password strategy
    
    Since credentials will only contain UID when a public application is calling, the fallback method of finding by UID alone is dead code. Private apps are not allowed to be identified by UID alone.

--- spec/dummy/db/migrate/20111122132257_create_users.rb
+++ spec/dummy/db/migrate/20111122132257_create_users.rb
@@ -1,4 +1,6 @@
-class CreateUsers < ActiveRecord::Migration
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[4.2]
   def change
     create_table :users do |t|
       t.string :name
