6b56666fc91339260bfe2260cef73e743b17bbec
Nikita Bulai <bulaj.nikita@gmail.com>
2019-03-28 12:00:20 +0300

    Fix StaleRecordsCleaner
    Fix StaleRecordsCleaner to work with bit amount of records without
    stopping the database with huge queries.

--- lib/doorkeeper/orm/active_record/stale_records_cleaner.rb
+++ lib/doorkeeper/orm/active_record/stale_records_cleaner.rb
@@ -3,22 +3,29 @@
 module Doorkeeper
   module Orm
     module ActiveRecord
+      # Helper class to clear stale and non-active tokens and grants.
+      # Used by Doorkeeper Rake tasks.
+      #
       class StaleRecordsCleaner
         def initialize(base_scope)
           @base_scope = base_scope
         end
 
+        # Clears revoked records
         def clean_revoked
           table = @base_scope.arel_table
+
           @base_scope.where.not(revoked_at: nil)
             .where(table[:revoked_at].lt(Time.current))
-            .delete_all
+            .in_batches(&:delete_all)
         end
 
+        # Clears expired records
         def clean_expired(ttl)
           table = @base_scope.arel_table
+
           @base_scope.where(table[:created_at].lt(Time.current - ttl))
-            .delete_all
+            .in_batches(&:delete_all)
         end
       end
     end
