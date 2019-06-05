d78c0b8162dc86660eef91e7acfedb6620a4f223
Sergey Tokarenko <private.tokarenko.sergey@gmail.com>
2019-06-05 19:03:35 +0300

    Use strong parameters for pre-authorization

--- app/controllers/doorkeeper/authorizations_controller.rb
+++ app/controllers/doorkeeper/authorizations_controller.rb
@@ -68,7 +68,12 @@ module Doorkeeper
     def pre_auth
       @pre_auth ||= OAuth::PreAuthorization.new(Doorkeeper.configuration,
                                                 server.client_via_uid,
-                                                params)
+                                                pre_auth_params)
+    end
+
+    def pre_auth_params
+      params.permit(:response_type, :redirect_uri, :scope, :state,
+                    :code_challenge, :code_challenge_method)
     end
 
     def authorization
