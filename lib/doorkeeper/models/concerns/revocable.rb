4e102f33f5d8be18fe696ce9dbf65335480e65a9
trit@seek.com.au <>
2020-01-20 12:10:16 +1100

    Use secret_strategy before by_refresh_token

--- lib/doorkeeper/models/concerns/revocable.rb
+++ lib/doorkeeper/models/concerns/revocable.rb
@@ -39,8 +39,10 @@ module Doorkeeper
       #   Access Token record or nil if nothing found
       #
       def old_refresh_token
+        plaintext_previous_refresh_token = Doorkeeper.config.token_secret_strategy.restore_secret(self, :previous_refresh_token)
+
         @old_refresh_token ||=
-          Doorkeeper.config.access_token_model.by_refresh_token(previous_refresh_token)
+          Doorkeeper.config.access_token_model.by_refresh_token(plaintext_previous_refresh_token)
       end
 
       def refresh_token_revoked_on_use?
