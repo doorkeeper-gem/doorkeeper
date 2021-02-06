da9e3c6a464e225734a7963ab4a4ae2a9c59b6bb
Nikita Bulai <bulaj.nikita@gmail.com>
2021-02-06 14:15:52 +0300

    Simplify TokenInfoController to be overridable (extract response rendering)
    Relates to #1466

--- app/controllers/doorkeeper/token_info_controller.rb
+++ app/controllers/doorkeeper/token_info_controller.rb
@@ -4,12 +4,22 @@ module Doorkeeper
   class TokenInfoController < Doorkeeper::ApplicationMetalController
     def show
       if doorkeeper_token&.accessible?
-        render json: doorkeeper_token, status: :ok
+        render json: doorkeeper_token_to_json, status: :ok
       else
         error = OAuth::InvalidTokenResponse.new
         response.headers.merge!(error.headers)
-        render json: error.body, status: error.status
+        render json: error_to_json(error), status: error.status
       end
+    end
+
+    protected
+
+    def doorkeeper_token_to_json
+      doorkeeper_token
+    end
+
+    def error_to_json(error)
+      error.body
     end
   end
 end
