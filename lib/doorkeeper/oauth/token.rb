0bbe69b1df6940e76492984dad3006764ad1c728
gkemmey <gray.kemmey@gmail.com>
2026-02-17 14:32:42 -0500

    add `OAuth::Token.resolve` returning access token resolution details from request
    in anticipation of needing details about how an access token was resolved from
    the request, this adds a `resolve` method that returns an
    `OAuth::Token::Resolution` object with `access_token_method`, `plaintext_token`,
    and `access_token` readers.
    
    the public `authenticate` method that just returns the `access_token` is
    preserved, and is implemented by delegating to `resolve`.

--- lib/doorkeeper/oauth/token.rb
+++ lib/doorkeeper/oauth/token.rb
@@ -3,6 +3,8 @@
 module Doorkeeper
   module OAuth
     class Token
+      Resolution = Struct.new(:access_token_method, :plaintext_token, :access_token)
+
       class << self
         def from_request(request, *methods)
           methods.inject(nil) do |_, method|
@@ -12,14 +14,21 @@ module Doorkeeper
           end
         end
 
-        def authenticate(request, *methods)
-          if (token = from_request(request, *methods))
+        def resolve(request, *methods)
+          method, token = methods.lazy.map { |m| [m, from_request(request, m)] }.detect(&:last)
+
+          if token
             access_token = Doorkeeper.config.access_token_model.by_token(token)
             if access_token.present? && Doorkeeper.config.refresh_token_enabled?
               access_token.revoke_previous_refresh_token!
             end
-            access_token
           end
+
+          Resolution.new(method, token, access_token) if access_token
+        end
+
+        def authenticate(request, *methods)
+          resolve(request, *methods)&.access_token
         end
 
         def from_access_token_param(request)
